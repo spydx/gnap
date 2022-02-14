2ccc7511be6940b63d5fd3ed5cadaa5f865d17ef
Kenneth Fossen <fossen.kenneth@gmail.com>
2022-02-14 19:19:14 +0100

    authentication

---
+++ gnap_client/src/gnap_session.rs
@@ -0,0 +1,34 @@
+use model::gnap::GnapOptions;
+
+pub struct GnapSession {
+    pub access_token: Option<String>,
+    pub tx_contiune: Option<String>,
+    pub instance_id: Option<String>,
+    pub redirect: Option<String>,
+    pub nonce: Option<String>,
+    pub options: Option<GnapOptions>,
+}
+
+impl GnapSession {
+    pub fn new() -> Self {
+        Self {
+            access_token: None,
+            tx_contiune: None,
+            instance_id: None,
+            redirect: None,
+            nonce: None,
+            options: None,
+        }
+    }
+
+    pub fn new_with_options(options: GnapOptions) -> Self {
+        Self {
+            access_token: None,
+            tx_contiune: None,
+            instance_id: None,
+            redirect: None,
+            nonce: None,
+            options: Some(options),
+        }
+    }
+}
